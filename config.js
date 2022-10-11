2bebe33640c77a6dc8f988397ec4f457940cccde
DewaSangatKull <115514681+DewaSangatKull@users.noreply.github.com>
2022-10-11 08:47:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6289629733278','6289629733278','6289629733278']
-global.premium = ['6289629733278']
+global.owner = ['628980321616','628980321616','628980321616']
+global.premium = ['628980321616']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
